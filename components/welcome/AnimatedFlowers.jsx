69b0e315a261ecf2a04c9528a826bd7dacc0e40f
unknown <alkristy7@gmail.com>
2025-11-25 22:34:14 +0100

    edits on flower positons on mobile

--- components/welcome/AnimatedFlowers.jsx
+++ components/welcome/AnimatedFlowers.jsx
@@ -2,26 +2,26 @@
 
 export default function AnimatedFlowers() {
   return (
-    <div className="flex-1 relative w-full h-96 md:h-full mt-12 md:mt-0 pointer-events-none">
+    <div className="flex-1 relative w-full h-96 md:h-full mt-12 md:mt-0 pointer-events-none overflow-visible">
       {/* Top-right flower */}
       <img
-        src="/flower3.svg"
+        src="/flower.svg"
         alt=""
-        className="absolute w-70 h-70 top-4 right-4 md:w-80 md:h-80 md:top-16 md:right-10 animate-blob hover:scale-105 transition-transform duration-500"
+        className="absolute w-72 h-72 -top-4 -right-10 md:w-80 md:h-80 md:top-16 md:right-10 animate-blob hover:scale-105 transition-transform duration-500"
       />
 
       {/* Bottom-left flower */}
       <img
-        src="/flower2.svg"
+        src="/flower.svg"
         alt=""
-        className="absolute w-40 h-40 bottom-8 left-8 md:w-64 md:h-64 md:bottom-28 md:left-1/4 animate-blob animation-delay-1500 hover:scale-105 transition-transform duration-500"
+        className="absolute w-36 h-36 -bottom-1 -left-4 md:w-64 md:h-64 md:bottom-28 md:left-1/4 animate-blob animation-delay-1500 hover:scale-105 transition-transform duration-500"
       />
 
       {/* Center-right flower */}
       <img
-        src="/flower4.svg"
+        src="/flower.svg"
         alt=""
-        className="absolute w-32 h-32 top-1/4 left-1/2 md:w-40 md:h-40 md:top-1/3 md:left-2/3 animate-blob animation-delay-3000 hover:scale-105 transition-transform duration-500"
+        className="absolute w-40 h-40 top-28 left-1/2 md:w-40 md:h-40 md:top-1/3 md:left-2/3 animate-blob animation-delay-3000 hover:scale-105 transition-transform duration-500"
       />
 
       <style jsx>{`
