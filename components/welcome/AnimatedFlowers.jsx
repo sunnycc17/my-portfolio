7d79b2c48313f8e068bfe41b3e8b84032fdea619
unknown <alkristy7@gmail.com>
2025-11-25 22:17:08 +0100

    edits on animated flower component changing svgs postions and sizes

--- components/welcome/AnimatedFlowers.jsx
+++ components/welcome/AnimatedFlowers.jsx
@@ -3,22 +3,25 @@
 export default function AnimatedFlowers() {
   return (
     <div className="flex-1 relative w-full h-96 md:h-full mt-12 md:mt-0 pointer-events-none">
+      {/* Top-right flower */}
       <img
-        src="/flower2.svg"
+        src="/flower3.svg"
         alt=""
-        className="absolute w-80 h-80 top-10 right-10 animate-blob hover:scale-105 transition-transform duration-500"
+        className="absolute w-70 h-70 top-4 right-4 md:w-80 md:h-80 md:top-16 md:right-10 animate-blob hover:scale-105 transition-transform duration-500"
       />
 
+      {/* Bottom-left flower */}
       <img
         src="/flower2.svg"
         alt=""
-        className="absolute w-64 h-64 bottom-20 left-1/4 animate-blob animation-delay-1500 hover:scale-105 transition-transform duration-500"
+        className="absolute w-40 h-40 bottom-8 left-8 md:w-64 md:h-64 md:bottom-28 md:left-1/4 animate-blob animation-delay-1500 hover:scale-105 transition-transform duration-500"
       />
 
+      {/* Center-right flower */}
       <img
-        src="/flower2.svg"
+        src="/flower4.svg"
         alt=""
-        className="absolute w-40 h-40 top-1/3 left-2/3 animate-blob animation-delay-3000 hover:scale-105 transition-transform duration-500"
+        className="absolute w-32 h-32 top-1/4 left-1/2 md:w-40 md:h-40 md:top-1/3 md:left-2/3 animate-blob animation-delay-3000 hover:scale-105 transition-transform duration-500"
       />
 
       <style jsx>{`
