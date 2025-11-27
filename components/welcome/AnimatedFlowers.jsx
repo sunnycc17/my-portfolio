8c082c5a4523ee125b8968f510a8eeaf6564d802
unknown <alkristy7@gmail.com>
2025-11-27 14:08:02 +0100

    added drop shadow on the flowers

--- components/welcome/AnimatedFlowers.jsx
+++ components/welcome/AnimatedFlowers.jsx
@@ -5,23 +5,23 @@ export default function AnimatedFlowers() {
     <div className="flex-1 relative w-full h-96 md:h-full mt-12 md:mt-0 pointer-events-none overflow-visible">
       {/* Top-right flower */}
       <img
-        src="/flower.svg"
+        src="/flower2.svg"
         alt=""
-        className="absolute w-60 h-60 -top-4 -right-8 md:w-80 md:h-80 md:top-16 md:right-10 animate-blob hover:scale-105 transition-transform duration-500 drop-shadow-2xl"
+        className="absolute w-60 h-60 -top-4 -right-8 md:w-80 md:h-80 md:top-16 md:right-10 animate-blob hover:scale-105 transition-transform duration-500 drop-shadow-sm drop-shadow-gray-700 "
       />
 
       {/* Bottom-left flower */}
       <img
-        src="/flower.svg"
+        src="/flower2.svg"
         alt=""
-        className="absolute w-28 h-28 bottom-1 -right-4 md:w-64 md:h-64 md:bottom-28 md:left-1/4 animate-blob animation-delay-1500 hover:scale-105 transition-transform duration-500 drop-shadow-2xl"
+        className="absolute w-28 h-28 bottom-1 -right-4 md:w-64 md:h-64 md:bottom-28 md:left-1/4 animate-blob animation-delay-1500 hover:scale-105 transition-transform duration-500 drop-shadow-sm drop-shadow-gray-700"
       />
 
       {/* Center-right flower */}
       <img
-        src="/flower.svg"
+        src="/flower2.svg"
         alt=""
-        className="absolute w-36 h-36 top-28 right-1/2 md:w-40 md:h-40 md:top-5/12 md:left-2/3 animate-blob animation-delay-3000 hover:scale-105 transition-transform duration-500 drop-shadow-2xl"
+        className="absolute w-36 h-36 top-28 right-1/2 md:w-40 md:h-40 md:top-5/12 md:left-2/3 animate-blob animation-delay-3000 hover:scale-105 transition-transform duration-500 drop-shadow-sm drop-shadow-gray-700"
       />
 
       <style jsx>{`
