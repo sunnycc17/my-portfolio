d63b040fb659cc86ee267fd30408cded99d01442
unknown <alkristy7@gmail.com>
2025-12-02 12:11:50 +0100

    further edits on the flower size and positon

--- components/welcome/AnimatedFlowers.jsx
+++ components/welcome/AnimatedFlowers.jsx
@@ -2,26 +2,26 @@
 
 export default function AnimatedFlowers() {
   return (
-    <div className="relative flex justify-center items-center w-full max-w-[700px] h-[150px] md:h-[520px] mx-auto pointer-events-none overflow-visible">
+    <div className="relative flex justify-center items-center w-full max-w-[300px] h-[200px] md:h-[520px] md:[700px] mx-auto pointer-events-none overflow-visible">
       {/* Top-right */}
       <img
         src="/flower2.svg"
         alt=""
-        className="absolute w-40 h-40 md:w-80 md:h-80 -top-3 -right-6 md:top-0 md:right-0 animate-blob drop-shadow-sm drop-shadow-black"
+        className="absolute w-56 h-56 md:w-80 md:h-80 -top-20 -right-10 md:top-0 md:right-0 animate-blob drop-shadow-sm drop-shadow-black"
       />
 
       {/* Bottom-left */}
       <img
         src="/flower2.svg"
         alt=""
-        className="absolute w-20 h-20 bottom-8 -right-4 md:w-60 md:h-60 md:bottom-15 md:left-40 animate-blob animation-delay-1500 drop-shadow-sm drop-shadow-black"
+        className="absolute w-28 h-28 bottom-8 -right-4 md:w-60 md:h-60 md:bottom-15 md:left-40 animate-blob animation-delay-1500 drop-shadow-sm drop-shadow-black"
       />
 
       {/* Mid-right */}
       <img
         src="/flower2.svg"
         alt=""
-        className="absolute w-32 h-32 top-15 left-16 md:w-40 md:h-40 md:top-6/12 md:left-2/3 animate-blob animation-delay-3000 drop-shadow-sm drop-shadow-black"
+        className="absolute w-36 h-36 top-15 left-4 md:w-40 md:h-40 md:top-6/12 md:left-2/3 animate-blob animation-delay-3000 drop-shadow-sm drop-shadow-black"
       />
       <style jsx>{`
         @keyframes blob {
